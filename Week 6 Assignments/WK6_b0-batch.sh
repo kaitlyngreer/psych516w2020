8936916b3e680d602c50f1266bbd9d4a4c5d98ad
kaitlyngreer <59666149+kaitlyngreer@users.noreply.github.com>
2020-02-25 22:33:54 -0700

    Week 6 adjusted assignments

--- Week 6 Assignments/WK6_b0-batch.sh
+++ Week 6 Assignments/WK6_b0-batch.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
+
 curTime=`date +"%Y%m%d-%H%M%S"`
 mkdir -p ~/logfiles/EDSD/${curTime}-b0
-for SUBJID in $(ls ~/compute/psych516/Data/BIDS/EDSD/sub-*); do
+
+for SUBJID in $(ls ~/compute/psych516/Data/derivative/EDSD/pT1/); do
   sbatch \
   -o ~/logfiles/EDSD/${curTime}-b0/o-${SUBJID}.txt \
   -e ~/logfiles/EDSD/${curTime}-b0/e-${SUBJID}.txt \
   ~/compute/psych516/scripts/EDSD/b0-job.sh \
+  ${SUBJID}
   sleep 1
 done
--- Week 6 Assignments/b0-job.sh
+++ Week 6 Assignments/b0-job.sh
@@ -5,24 +5,22 @@
 #SBATCH --nodes=1   # number of nodes
 #SBATCH --mem-per-cpu=16G  # memory per CPU core
 
-SUBJID=ls ~/compute/psych516/Data/derivative/EDSD/pT1
-
 #LOAD ENVIRONMENTAL VARIABLES
 ~/research_bin/FSL/bin/
 
 #CREATE DERIVATIVE DIRECTORY
-mkdir -p ~/compute/psych516/Data/derivative/EDSD/pDWI/${SUBJID}
+mkdir -p ~/compute/psych516/Data/derivative/EDSD/pDWI/${1}/
 
 #SYMLINK VARIABLES
-ln -s ~/compute/psych516/Data/BIDS/EDSD/${SUBJID}/dwi/${SUBJID}_dwi.nii ~/compute/psych516/Data/derivative/EDSD/pDWI/${SUBJID}/dwi.nii.gz
+ln -s ~/compute/psych516/Data/BIDS/EDSD/${1}/dwi/${1}_dwi.nii ~/compute/psych516/Data/derivative/EDSD/pDWI/${1}/dwi.nii.gz
 
-ln -s ~/compute/psych516/Data/BIDS/EDSD/${SUBJID}/dwi/${SUBJID}_dwi.bval ~/compute/psych516/Data/derivative/EDSD/pDWI/${SUBJID}/dwi.bval
+ln -s ~/compute/psych516/Data/BIDS/EDSD/${1}/dwi/${1}_dwi.bval ~/compute/psych516/Data/derivative/EDSD/pDWI/${1}/dwi.bval
 
-ln -s ~/compute/psych516/Data/BIDS/EDSD/${SUBJID}/dwi/${SUBJID}_dwi.bvec ~/compute/psych516/Data/derivative/EDSD/pDWI/${SUBJID}/dwi.bvec
+ln -s ~/compute/psych516/Data/BIDS/EDSD/${1}/dwi/${1}_dwi.bvec ~/compute/psych516/Data/derivative/EDSD/pDWI/${1}/dwi.bvec
 
 #B0 EXTRACTION AND BRAIN MASK
-cd ~/compute/psych516/Data/derivative/EDSD/pDWI/${SUBJID}
+workdir=~/compute/psych516/Data/derivative/EDSD/pDWI/${1}/
 
-fslroi dwi.nii.gz dwi_b0.nii.gz 0 1
+fslroi ${workdir}/dwi.nii.gz ${workdir}/dwi_b0.nii.gz 0 1
 
-bet dwi_b0.nii.gz dwi_b0 -f 0.1 -g 0 -m -n
+bet ${workdir}/dwi_b0.nii.gz ${workdir}/dwi_b0 -f 0.1 -g 0 -m -n
